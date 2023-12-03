cc2f6c40de8d6cac790c8881ea3796e44ad55100
trieuld <letrieu106@gmail.com>
2023-12-03 20:03:58 +0700

    README

--- update_readme.sh
+++ update_readme.sh
@@ -2,6 +2,7 @@
 
 # Get the current time and format it to date and time
 update_at="**Update At:** $(date +'%d-%m-%Y')"
+current_time=$(date +'%d-%m-%Y')
 
 # Replace the old content in README.md with the new time
 sed -i "s/\*\*Update At:\*\*.*$/$update_at/" README.md
@@ -10,7 +11,7 @@ sed -i "s/\*\*Update At:\*\*.*$/$update_at/" README.md
 git add README.md
 
 # Commit the changes
-git commit -m "Update At: $update_at"
+git commit -m "Update At: $current_time"
 
 # Continue with the commit
 exit 0
